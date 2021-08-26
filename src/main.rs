e4deef32735a69c9b0af9d38e9a6205c36801307
Npg <86592522+Wumpuspro@users.noreply.github.com>
2021-08-26 07:30:18 +0545

    learning rust language

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::env;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*
+};
+
+const HELP_COMMAND: &str = "!help";
+const HELP_MESSAGE: &str = "
+Hello there, Human!
+You have summoned me. Let's see about getting you what you need.
+? Need technical help ?
+=> Post in the <#channel_id> channel and other humans will assist you.
+? Looking for the Code of Conduct ?
+=> Here it is: <https://opensource.facebook.com/code-of-conduct>
+? Something wrong ?
+=> You can flag and admin with <@&role_id>
+I hope that resolves your issue!
+-- Helpbot
+";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message {:?}", why);
+            }
+        }
+    }
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
